862c19bc76f46f9dc31fb266b2747aa1b4ed172e
SOUTA　SUZUKI <soutasuzuki1101@gmail.com>
2026-08-25 13:30:07 +0900

    fix(pwa): force aggressive reload on sw kill-switch

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,17 @@
-self.addEventListener('install', (e) => { self.skipWaiting(); }); self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim().then(() => { self.registration.unregister(); })); });
+self.addEventListener('install', (e) => { 
+    self.skipWaiting(); 
+}); 
+
+self.addEventListener('activate', (e) => { 
+    e.waitUntil(
+        self.clients.claim().then(() => { 
+            return self.registration.unregister(); 
+        }).then(() => {
+            return self.clients.matchAll({ type: 'window' });
+        }).then((windowClients) => {
+            for (let client of windowClients) {
+                client.navigate(client.url);
+            }
+        })
+    ); 
+});
